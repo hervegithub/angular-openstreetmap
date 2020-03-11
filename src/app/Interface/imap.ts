15355cc3d9d8f19b0c30fce63cdf6c22558f64ef
Mohamed Achraf el Hamdaoui <med.achraf.hamdaoui@gmail.com>
2020-03-11 01:04:50 +0100

    interface pour les donnes des api

---
+++ src/app/Interface/imap.ts
@@ -0,0 +1,11 @@
+export interface IMap {
+    nom : string;
+    adresse : string;
+    description : string;
+    ville : string;
+    pays : string;
+    type : string;
+    latitude :string;
+    longitude :string;
+    image : Blob;
+}
